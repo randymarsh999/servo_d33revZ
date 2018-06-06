80ebb6d5437a2768a6e21a29ba1cc07565b8de12
vortexblow <vortexblow@gmail.com>
2018-06-06 15:03:30 +0300

    initial code

---
+++ servo_d33rev/servo_d33rev/servo_d33rev.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		servo_d33rev.ino
+ Created:	06.06.2018 15:02:22
+ Author:	root
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
